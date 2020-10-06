22c66facd59d332e0e330c4168efe830d5be37c1
Akshaytyag <you@example.com>
2020-10-06 13:29:17 +0530

    Updates

---
+++ precache-manifest.69444bb3bd463731ba19334d037d03ef.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20ffebc6f2113bd040a077c79fd0583b",
+    "url": "/Quiz/index.html"
+  },
+  {
+    "revision": "a7e8d78d891e4a0f05fb",
+    "url": "/Quiz/static/css/2.c1317ec3.chunk.css"
+  },
+  {
+    "revision": "f33236871a72b08229bc",
+    "url": "/Quiz/static/css/main.de150c94.chunk.css"
+  },
+  {
+    "revision": "a7e8d78d891e4a0f05fb",
+    "url": "/Quiz/static/js/2.77e61e9d.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Quiz/static/js/2.77e61e9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33236871a72b08229bc",
+    "url": "/Quiz/static/js/main.4844de54.chunk.js"
+  },
+  {
+    "revision": "5ea3d099ecad1633f71a",
+    "url": "/Quiz/static/js/runtime-main.298ff0f6.js"
+  },
+  {
+    "revision": "acab4c7b8382ac447d843bc80bf5bcc4",
+    "url": "/Quiz/static/media/Quiz.acab4c7b.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Quiz/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Quiz/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Quiz/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Quiz/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Quiz/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
